36e8eaaaf2a2129993f602bdcfbf6d8805f389d7
arki <antoniorafaelariasromero@gmail.com>
2025-02-04 23:01:22 -0500

    feat(cli): add initial CLI implementation

---
+++ include/cli.h
@@ -0,0 +1,28 @@
+#ifndef CLI_H
+#define CLI_H
+#define MAX_FLAGS 16
+#define MAX_VALUES 16
+
+typedef struct {
+    char **flags;
+    int flag_count;
+    char **values;
+    int values_count;
+} CLI;
+
+CLI* cli_init();
+
+void cli_parse(CLI* cli, int argc, char* argv[]);
+
+void cli_add_flag(CLI* cli, const char* name, const char* short_name);
+
+char* cli_get_flag_value(CLI* cli, const char* name);
+
+bool cli_has_flag(CLI* cli, const char* flag);
+
+char* cli_get_value(CLI* cli, int index);
+
+void cli_free(CLI* cli);
+
+#endif
+#endif
---
+++ src/cli.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "parse.h"
+#include "utils.h"
+#include <stdlib.h>
+#include "cli.h"
+
